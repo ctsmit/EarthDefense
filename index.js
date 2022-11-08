8c9a0d3989c26c098ce47f69d8c5c9cd0f23474a
ctsmit <114516481+ctsmit@users.noreply.github.com>
2022-11-08 14:01:23 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let player = document.querySelector(".player")
+player = {
+
+}
+
+
+
+
+
+class AlienBomber {
+   constructor(amount,round) {
+      
+   }
+   move(round){
+
+   }
+}
+
+document.onkeydown = function(e) {
+   switch (e.key) {
+      case "ArrowLeft":
+         alert('left')
+         break
+      case "a":
+         alert("aleft")
+         break
+      case "ArrowRight" || "D":
+         alert('right')
+         break
+      case "d":
+         
+   }
+};
+
